7e34d24ac7131dcab5b43584da2e9fa90bd1947a
1M0NsR3P0 <naimonchy@gmail.com>
2025-07-21 20:16:02 +0600

    first commit

---
+++ server.py
@@ -0,0 +1,20 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    uri = "mongodb+srv://imon:imon55@c0.ckcrbdq.mongodb.net/?retryWrites=true&w=majority&appName=C0"
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    try:
+        client.admin.command('ping')
+        db = client["mydatabase"]
+        collection = db["mycollection"]
+        return "Connected to MongoDB and accessed collection successfully!"
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run()
